a0cfdc4ce68344732c974c570e6a026ac9abaab0
noble_gasses <nicholascoiro@gmail.com>
2018-12-03 14:49:37 -0300

    Solved day 3b
    the website doesn't like #

--- Day 3b.py
+++ Day 3b.py
@@ -10,7 +10,6 @@ import numpy
 fabric = Functions.read_file("Day3list.txt")
 #fabric = Functions.read_file("debug3.txt")
 chopped = []
-
 total = []
 for x in range (len(fabric)):
     chopped.append(fabric[x].split('@'))
@@ -28,9 +27,9 @@ for a in range (len(chopped)):
     for x in range (int(chopped[a][1][1][1])):
         for y in range (int (chopped[a][1][1][0])):
             if sheet[x+chopped[a][1][0][1], y+chopped[a][1][0][0]] == 0:
-                sheet[x+chopped[a][1][0][1], y+chopped[a][1][0][0]] = chopped[0]
+                sheet[x+chopped[a][1][0][1], y+chopped[a][1][0][0]] = a
             elif sheet[x+chopped[a][1][0][1], y+chopped[a][1][0][0]] != 0:
-                sheet[x+chopped[a][1][0][1], y+chopped[a][1][0][0]] = "x"
+                sheet[x+chopped[a][1][0][1], y+chopped[a][1][0][0]] = 20000
 
 """## convert your array into a dataframe
 df = pd.DataFrame (sheet)
@@ -41,11 +40,17 @@ filepath = 'sheet.xlsx'
 
 df.to_excel(filepath, index=False)"""
 
-print (sheet)
+print (str(sheet))
 for a in range (len(chopped)):
+    total.append(0)
     for x in range (size):
         for y in range (size):
-            if sheet.item((x,y)) == :
-                total = total + 1
-
-print (total)
+            if sheet.item((x,y)) == a:
+                total[a] = total[a] + 1
+    if total [a]== chopped[a][1][1][1] * chopped[a][1][1][0]:
+        print (total[a])
+        choppedtotal= (chopped[a][1][1][1] * chopped[a][1][1][0])
+        print (str(chopped[a][1][1][1])+' * '+str(chopped[a][1][1][0])+ ' = ' + str(choppedtotal))
+        ans = chopped[a][0]
+        break
+print (ans)
