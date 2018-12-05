bbe9ae5bcc27bd1d3ae828884dee3d9c4239f1e4
noble_gasses <nicholascoiro@gmail.com>
2018-12-05 15:50:08 -0300

    completed day 4 a
    took me all day but day 4a is finished!

--- day 4 read the prompt.py
+++ day 4 read the prompt.py
@@ -26,16 +26,77 @@ for x in range (len(shifts)):
         chopped[x][1][1] = chopped[x][1][1].strip (" Guard #")
         chopped[x][1][1] = chopped[x][1][1].strip (" begins shift")
 chopped.sort()
+
+
 for x in range (len(chopped)):
     if chopped [x][1][1]=="s":
         sleep = int(chopped[x][1][0][1])
     elif chopped [x][1][1]=="w":
         total = total + int(chopped[x][1][0][1]) - sleep
-    else:
-        current_gaurd = chopped[x][1][0][1]
-        gaurds.append([current_gaurd,"placeholder"])
-        gaurds[i][1] = total
-        i= i+1
+        gaurds[i-1][1] = int(gaurds[i-1][1])+total
         total= 0
+    else:
+        current_gaurd = chopped[x][1][1]
+        gaurds.append([current_gaurd,0])
+        i= i+1
+
+
+gaurds.sort()
+i=0
+current_gaurd = gaurds[0][0]
+gaurdtime = [[gaurds[0][0],0]]
+for x in range (len(gaurds)):
+    if gaurds[x][0] == current_gaurd:
+        gaurdtime[i][1] = int(gaurdtime[i][1]) + int(gaurds[x][1])
+    elif gaurds[x][0] != current_gaurd:
+        current_gaurd = gaurds[x][0]
+        gaurdtime.append(gaurds[x])
+        i=i+1
         
-print (gaurd)
+gaurdtime.sort(key = lambda x: x[1],reverse= True)
+i=0
+timeschedule = []
+tracking = False
+target = gaurdtime[0][0] 
+gaurdcheck= 0
+for x in range (len(chopped)):
+    if chopped[x][1][1] == target:
+        if gaurdcheck == target:
+            i=i+1
+        gaurdcheck = chopped[x][1][1]
+        tracking = True
+        timeschedule.append([chopped[x+1][0],target,[]])
+    if chopped[x][1][1] == 's' and tracking == True:
+        sleep = chopped[x][1][0][1]
+    if chopped[x][1][1] == 'w' and tracking == True:
+        time = int (chopped[x][1][0][1]) - int (sleep)
+        for y in range (time):
+          timeschedule[i][2].append(y+int(sleep))
+    if chopped[x][1][1] != target and chopped[x][1][1] !='w' and chopped[x][1][1] !='s' and tracking == True:
+        tracking = False
+        gaurdcheck = chopped[x][1][1]
+        i= i + 1
+frequency = []
+for x in range (60):
+    frequency.append([x,0])
+    for y in range(len(timeschedule)):
+        for z in range (len (timeschedule[y][2])):
+            if x== timeschedule[y][2][z]:
+                frequency[x][1] = int (frequency[x][1]) + 1
+frequency.sort(key = lambda x: x[1],reverse= True)
+#print (frequency)
+#print (target)
+gah = int(frequency [0][0]) * int(target)
+print (gah)
+#print (timeschedule)
+
+#print ("\n\n\n\n\n\n")
+#print (chopped)"""
+#print (gaurdtime)
+#print (chopped)
+#print (gaurds)
+counter = 0
+"""for x in range (len (gaurds)):
+    if chopped[x][0]== '5':
+        counter = counter +1
+print (counter)"""
